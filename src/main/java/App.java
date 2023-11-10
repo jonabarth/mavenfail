ef2e3163da97e83e894410d2ae2fc9f8be5e04b5
Jonathan Barth <jbarth@atlassian.com>
2023-11-10 16:54:42 +0100

    asd

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    // This method will cause a compile error because it does not return a value
+    public static int failMethod() {
+        // No return statement
+    }
+}
